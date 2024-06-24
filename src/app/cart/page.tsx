11840c7292d120f9560ca1eff7d4cecd5770bb4b
Kittipakorn <kittipakorn1234@gmail.com>
2024-06-24 22:45:40 +0700

    fixed bug

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -9,7 +9,6 @@ import Link from "next/link";
 export default function CartPage() {
   const { cart, getTotal } = useCart();
 
-  //paymants
   const [qrCode, setQrCode] = useState<string | null>(null);
   const [showModal, setShowModal] = useState(false);
 
@@ -37,9 +36,16 @@ export default function CartPage() {
     <div>
       <div className="bg-slate-200 min-h-[100vh]">
         <Link href="/">
-        <div className="pl-5 pt-5">
-         <svg className="w-10" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#000000" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"></path><path fill="#000000" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"></path></g></svg>
-        </div>
+          <div className="pl-5 pt-5">
+            <svg className="w-10" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#000000">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path fill="#000000" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"></path>
+                <path fill="#000000" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"></path>
+              </g>
+            </svg>
+          </div>
         </Link>
         <h1 className="text-center p-5 font-bold text-4xl">Your cart</h1>
         {cart.length === 0 ? (
@@ -47,7 +53,7 @@ export default function CartPage() {
         ) : (
           <div className="grid grid-rows gap-4 p-10">
             {cart.map((item) => (
-              <div className="flex gap-5 bg-white rounded-lg p-5">
+              <div key={item.id} className="flex gap-5 bg-white rounded-lg p-5">
                 <img src={item.image} alt={item.title} className="w-24 object-contain" />
                 <div>
                   <h1>{item.title}</h1>
@@ -69,7 +75,7 @@ export default function CartPage() {
         </div>
       </div>
       <Modal show={showModal} onClose={handleCloseModal}>
-        {qrCode && <img src={qrCode} alt="QR Code for Payment" />}
+        {qrCode && <img src={qrCode} alt="QR Code for Payment" className="m-auto" />}
       </Modal>
     </div>
   );
